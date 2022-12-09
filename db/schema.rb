af70b19b7663fad55984ce3893092de0647a516b
Emilio Said <saidmacciseemilio@gmail.com>
2022-12-09 09:55:16 -0600

    solved

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_07_185054) do
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_181846) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -127,6 +127,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_181846) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
