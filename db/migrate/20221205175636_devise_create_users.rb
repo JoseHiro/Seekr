f5e34dd7c489fd49e8cf9db7992479a10fc2d388
Josey Sekine <ipho.7johero@icloud.com>
2022-12-09 08:18:30 -0600

    logged in pag desigen change

--- db/migrate/20221205175636_devise_create_users.rb
+++ db/migrate/20221205175636_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :first_name, null: false
-      t.string :last_name, nukk: false
+      t.string :last_name, null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = ["Automotive", "Computers & Electronics",
+"Education", "Food & Dining", "Health & Medicine", "Home & Garden", "Personal Care & Services",
+"Travel & Transportation"]
+
+
+User.destroy_all
+
+addresses = ["mexico city", "Bogota", "Lima","Tokyo"]
+10.times do
+  user = User.new(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name , password:111111, email:Faker::Internet.email)
+  user.save
+
+  business = Business.new(user_id: user.id, name:Faker::Restaurant.name, address:addresses.sample, category:categories.sample)
+  business.save
+  
+  2.times do
+    Product.create(business_id:business.id, name:Faker::Book.title, availability:[true,false].sample, price:100, category:categories.sample)
+  end
+
+  user.save
+  business.save
+end
