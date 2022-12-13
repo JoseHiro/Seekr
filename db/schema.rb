f5f3528fccd681791e2889b5ac64bffe171b7dba
Emilio Said <saidmacciseemilio@gmail.com>
2022-12-12 21:05:40 -0600

    standby

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_170051) do
     t.time "opening_time"
     t.time "closing_time"
     t.string "category"
-    t.boolean "open"
+    t.boolean "open", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
@@ -120,7 +120,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_170051) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
-    t.string "last_name"
+    t.string "last_name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -128,9 +128,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_170051) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
-    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
