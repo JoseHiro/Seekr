ba80ef4b32c5485e1f3c43923178b3bb4f2ab41a
Iskander Galbraith <ijgalbraith@protonmail.ch>
2022-12-15 14:40:51 -0600

    merge with master

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_163339) do
     t.time "opening_time"
     t.time "closing_time"
     t.string "category"
-    t.boolean "open", default: false
+    t.boolean "open"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
@@ -124,7 +124,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_163339) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -132,6 +132,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_163339) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
