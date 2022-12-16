b946e2b3df6479139e6dd24a99c34d6cf0a78f80
Josey Sekine <ipho.7johero@icloud.com>
2022-12-15 22:57:54 -0600

    landing body page cards positioning button balance size change

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_163339) do
     t.time "opening_time"
     t.time "closing_time"
     t.string "category"
-    t.boolean "open", default: false
+    t.boolean "open"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
@@ -124,7 +124,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_163339) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
